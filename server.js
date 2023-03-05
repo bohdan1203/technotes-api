c6049cbbf976411df1897802fa35296dfdc261e2
Bohdán Katsenko <80700199+bohdan1203@users.noreply.github.com>
2023-03-05 14:54:02 +0200

    Ready to deploy backend-3

--- server.js
+++ server.js
@@ -22,6 +22,16 @@ app.use(logger);
 
 app.use(cors(corsOptions));
 
+app.use((req, res, next) => {
+  res.header(
+    "Access-Control-Allow-Origin",
+    "https://technotes-unru.onrender.com"
+  );
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
+
 app.use(express.json());
 
 app.use(cookieParser());
